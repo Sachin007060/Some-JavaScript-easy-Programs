c93879cc4cf347f6370366a630193daa30e2df8b
Sachin007060 <sachinbaskandi00@gmail.com>
2024-03-31 18:11:43 +0530

    created demo.js file in the JSProgram folder

---
+++ demo.js
@@ -0,0 +1,27 @@
+let day = +prompt("Enter the day number: \n1.Mon \n2.Tue \n3.Wed \n4.Thr \n5.Fri \n6.Sat \n7.Sun");
+
+switch (day) {
+    case 7:
+        console.log("Hooray it's Weekend");
+        break;
+        case 1:
+            console.log("This is Weekday");
+            break;
+        case 2:
+            console.log("This is Weekday");
+            break;
+        case 3:
+            console.log("This is Weekday");
+            break;
+        case 4:
+            console.log("This is Weekday");
+            break;
+        case 5:
+            console.log("This is Weekday");
+            break;
+        case 6:
+            console.log("Hooray it's Weekend");
+            break;
+        default:
+            console.log("You enter the invalid opetion");
+}
